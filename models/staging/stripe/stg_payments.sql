f2d57edaaae35f61b32c5efb76029ce25f88ef9e
Marin Six <msix@ippon.fr>
2021-02-10 13:42:42 +0000

    updated test, docs and sources

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,10 @@
+select
+
+ID as payment_id,
+ORDERID	as order_id,
+PAYMENTMETHOD as payment_method,
+STATUS as payment_status,
+AMOUNT/100 as amount,
+CREATED	as created_at
+
+from {{ source('stripe', 'payment') }}
